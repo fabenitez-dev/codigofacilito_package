91538ff4ebad9d086836031d07434136b92e3c82
Francisco Benitez <francobenzsys@gmail.com>
2022-11-18 00:54:14 -0300

    modulo logging

--- pack_cfacilito/__main__.py
+++ pack_cfacilito/__main__.py
@@ -3,9 +3,20 @@
 # -m ejecuta por nombre de paquete
 
 from pack_cfacilito import unreleased
+import logging
+
+"""
+INFO -> 10
+DEBUG -> 20
+WARNING -> 30 - A partir de aqui para abajo por default
+ERROR -> 40 
+CRITICAL -> 50
+"""
+
+logging.basicConfig(level=logging.DEBUG) #que imprima mensajes de tipo DEBUG para abajo
 
 if __name__ == '__main__':
-    print('>>> Comienza ejecucion desde dentro de __main__.py\n')
+    logging.debug('>>> Comienza ejecucion desde dentro de __main__.py\n')
     workshops = unreleased()
     print(workshops)
-    print('\n>>> Finaliza ejecucion desde dentro de __main__.py')
+    logging.debug('\n>>> Finaliza ejecucion desde dentro de __main__.py')
