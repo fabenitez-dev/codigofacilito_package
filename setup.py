74ddca2e1c9251bc3bf23b1dc8c6fca756659f65
Francisco Benitez <francobenzsys@gmail.com>
2022-11-18 22:58:04 -0300

    borro emoticon readme

--- setup.py
+++ setup.py
@@ -1,23 +1,19 @@
-from pathlib import Path # > 3.6
+from pathlib import Path
 from setuptools import setup
-"""
-Este archivo es para que PyPi pueda clasificar la libreria correctamente y para que 
-pip pueda instalarla de forma apropiada
-"""
 
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text() #lee archivo README.md
+long_description = (this_directory/'README.md').read_text()
 
-VERSION = '0.0.1' # versionar como se desee 
+VERSION = '0.0.1'
 DESCRIPTION = 'Consume API de codigofacilito.com'
 PACKAGE_NAME = 'pack_cfacilito'
 AUTHOR = 'Francisco Alejandro Benitez'
 EMAIL = 'fabenitez.dev@hotmail.com'
-GITHUB_URL = 'https://github.com/fabenitez-dev/codigofacilito_package' #URL de github del paquete creado
+GITHUB_URL = 'https://github.com/fabenitez-dev/codigofacilito_package'
 
 setup(
     name = PACKAGE_NAME,
-    packages = [DESCRIPTION],
+    packages = [PACKAGE_NAME],
     version = VERSION,
     license='MIT',
     description = DESCRIPTION,
@@ -26,7 +22,9 @@ setup(
     author = AUTHOR,
     author_email = EMAIL,
     url = GITHUB_URL,
-    keywords = [],
+    keywords = [
+        'codigofacilito'
+    ],
     install_requires=[ 
         'requests',
     ],
